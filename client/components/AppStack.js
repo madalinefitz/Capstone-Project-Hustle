8a4ceea6ab1b6afb34a9dae4dedc8adf5427607a
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-04-27 18:04:48 -0400

    comment out appstack and authstack

--- client/components/AppStack.js
+++ client/components/AppStack.js
@@ -1,21 +1,21 @@
-import {View, Text} from 'react-native'
-import Home from './Home'
-import JobCategories from './JobCategories'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import {View, Text} from 'react-native'
+// import Home from './Home'
+// import JobCategories from './JobCategories'
+// import { SafeAreaView } from 'react-native-safe-area-context'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
 
-function AppStack ({navigation}){
-    return(
-        <SafeAreaView>
-            {/* <Home/>
-            <JobCategories/> */}
-            <AppStack/>
-        </SafeAreaView>
-    )
-}
+// function AppStack ({navigation}){
+//     return(
+//         <SafeAreaView>
+//             {/* <Home/>
+//             <JobCategories/> */}
+//             <AppStack/>
+//         </SafeAreaView>
+//     )
+// }
 
-export default AppStack
+// export default AppStack
--- client/components/AuthStack.js
+++ client/components/AuthStack.js
@@ -1,18 +1,18 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import {View, Text} from 'react-native'
-import Login from './Login'
+// import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// import { SafeAreaView } from 'react-native-safe-area-context'
+// import {View, Text} from 'react-native'
+// import Login from './Login'
 
-function AuthStack({navigation}){
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>Auth Stack</Text>
-                <Login/>
-                {/* <CreateAccount/> */}
-            </View>
-        </SafeAreaView>
-    )
-}
+// function AuthStack({navigation}){
+//     return(
+//         <SafeAreaView>
+//             <View>
+//                 <Text>Auth Stack</Text>
+//                 <Login/>
+//                 {/* <CreateAccount/> */}
+//             </View>
+//         </SafeAreaView>
+//     )
+// }
 
-export default AuthStack
+// export default AuthStack
