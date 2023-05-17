e364e10bc9c493ffaad2c3674599eb797480e371
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-05-16 22:31:07 -0400

    code updated so saturday shift shows in week

--- client/components/DateContext.js
+++ client/components/DateContext.js
@@ -9,7 +9,7 @@ export const DateProvider=({children})=> {
 
     const today = new Date()
     const first = today.getDate() - today.getDay()
-    const last = first + 6
+    const last = first + 7
 
     const firstday = new Date(today.setDate(first)).toDateString()
     const lastday = new Date(today.setDate(last)).toDateString()
